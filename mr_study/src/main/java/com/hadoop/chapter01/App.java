7bbc82baefc0d2085f70a33972d4de0d5adfee83
linxiaoJVM <120984linxiao>
2016-01-16 10:53:45 +0800

    hi, init project

---
+++ mr_study/src/main/java/com/hadoop/chapter01/App.java
@@ -0,0 +1,10 @@
+package com.hadoop.chapter01;
+
+/**
+ * Created by linxiao on 2016/1/16.
+ */
+public class App {
+    public static void main(String args[]) {
+        System.out.print("hello world");
+    }
+}
